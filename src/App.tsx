9b4c4f6768ac1b1047a2324903ddb7b3393eaafb
capsen-citycouncil <kaishen.lu@brisbane.qld.gov.au>
2025-01-02 19:49:26 +1000

    add auto support for dark theme base on system

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css'
 import FormComponent from './components/FormComponent'
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={theme} defaultMode='system'>
       <FormComponent />
-    </>
+    </ThemeProvider>
   )
 }
 
